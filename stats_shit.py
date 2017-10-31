81c7bb941c9934d9a1f7c595b5fb99be7b2b1c61
david phan <phanqdavid@gmail.com>
2017-10-31 14:29:17 -0700

    new argument option (#3)
    * refactored code using the scipy statistics module
    * added string argument option

--- stats_shit.py
+++ stats_shit.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 import sys
+import os.path
 import statistics as stats
 
-def main(num_txt_file):
-    with open(num_txt_file) as f:
-        nums = f.read().split()
-        numlst = list(float(num) for num in nums)
+def main(user_input):
 
-        n = len(numlst)
-        total = sum(numlst)
-        totsq = sum(num**2 for num in numlst)
-        median = stats.median(numlst)
-        mean = stats.mean(numlst)
-        stdvar = stats.variance(numlst)
-        stddev = stats.stdev(numlst)
+    if os.path.isfile(user_input):
+         with open(user_input) as f:
+            nums = f.read().split()
+    else:
+        nums = user_input.split()
 
-        print('n = {}'.format(n))
-        print('Sorted Data: {}'.format(' '.join(map(str, sorted(numlst)))))
-        print('Mean: {0:.4f}'.format(mean))
-        print('Median: {0:.4f}'.format(median))
-        print('Standard Variance: {0:.4f}'.format(stdvar))
-        print('Standard Deviation: {0:.4f}'.format(stddev))
-        print('Sum(Xi): {0:.4f}'.format(total))
-        print('Sum(Xi^2): {0:.4f}'.format(totsq))
+    numlst = list(float(num) for num in nums)
+    n = len(numlst)
+    total = sum(numlst)
+    totsq = sum(num**2 for num in numlst)
+    median = stats.median(numlst)
+    mean = stats.mean(numlst)
+    stdvar = stats.variance(numlst)
+    stddev = stats.stdev(numlst)
+
+    print('n = {}'.format(n))
+    print('Sorted Data: {}'.format(' '.join(map(str, sorted(numlst)))))
+    print('Mean: {0:.4f}'.format(mean))
+    print('Median: {0:.4f}'.format(median))
+    print('Standard Variance: {0:.4f}'.format(stdvar))
+    print('Standard Deviation: {0:.4f}'.format(stddev))
+    print('Sum(Xi): {0:.4f}'.format(total))
+    print('Sum(Xi^2): {0:.4f}'.format(totsq))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Usage: {} /path/to/num_set.txt'.format(sys.argv[0]), file=sys.stderr)
+        print('Usage1: {} /path/to/num_set.txt'.format(sys.argv[0]), file=sys.stderr)
+        print('Usage2: {} "2.0 3.0 4.0"'.format(sys.argv[0]), file=sys.stderr)
         exit(1)
     else:
         main(sys.argv[1])
